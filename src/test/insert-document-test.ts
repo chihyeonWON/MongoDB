5400d406f23dc69d3679f62209d37037acbe9475
원치현 <heungeob1003@gmail.com>
2021-11-16 14:16:19 +0900

    person, address 객체를 각각 personsCollection, addressesCollection에 저장하는 insert-document-test.ts 파일 생성

---
+++ src/test/insert-document-test.ts
@@ -0,0 +1,25 @@
+import {connect} from '../mongodb/connect'
+
+const insertDocumentTest = async() => {
+  let connection, cursor
+  try {
+    connection = await connect()
+    const db = await connection.db('mongodb') // mongodb라는 데이터베이스를 생성하는 코드
+    const personsCollection = collection.db('persons') // persons라는 이름을 가진 컬렉션 생성
+    const addressesCollection = collection.db('addresses') // addresses라는 이름을 가진 컬렉션 생성
+    
+    const person = {name:'Jack', age: 32}
+    let result = await personsCollection.insertOne(person)
+    console.log(result)
+
+    const address = {country: 'korea', city: 'seoul'}
+    result = await addressesCollection.insertOne(address)
+    console.log(result)
+  } catch(e) {
+    console.log(e.message)
+  } finally {
+    connection.close()
+  }  
+}
+
+insertDocumentTest()
