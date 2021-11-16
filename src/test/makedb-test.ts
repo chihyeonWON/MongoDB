a773905bd162908e41c9062b6b3de889a9891685
원치현 <heungeob1003@gmail.com>
2021-11-16 14:05:05 +0900

    persons, addresses 라는 이름의 두개의 컬렉션(collection)을 생성

--- src/test/makedb-test.ts
+++ src/test/makedb-test.ts
@@ -5,7 +5,9 @@ const connectTest = async() => {
   try {
     connection = await connect()
     const db = await connection.db('mongodb') // mongodb라는 데이터베이스를 생성하는 코드
-    console.log('mongodb', db)
+    const personsCollection = collection.db('persons') // persons라는 이름을 가진 컬렉션 생성
+    const addressesCollection = collection.db('addresses') // addresses라는 이름을 가진 컬렉션 생성
+    console.log(personsCollection, addressesCollection)
   } catch(e) {
     console.log(e.message)
   } finally {
