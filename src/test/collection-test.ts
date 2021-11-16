9df26c7f26e043f357a0b12fc85e8c139922044c
원치현 <heungeob1003@gmail.com>
2021-11-16 14:20:08 +0900

    persons, addresses라는 두개의 컬렉션을 만드는 collection-test.ts 파일 생성

---
+++ src/test/collection-test.ts
@@ -0,0 +1,18 @@
+import {connect} from '../mongodb/connect'
+
+const connectTest = async() => {
+  let connection
+  try {
+    connection = await connect()
+    const db = await connection.db('mongodb') // mongodb라는 데이터베이스를 생성하는 코드
+    const personsCollection = db.collection('persons') // persons라는 이름을 가진 컬렉션 생성
+    const addressesCollection = db.collection('addresses') // addresses라는 이름을 가진 컬렉션 생성
+    console.log(personsCollection, addressesCollection)
+  } catch(e) {
+    console.log(e.message)
+  } finally {
+    connection.close()
+  }  
+}
+
+connectTest()
